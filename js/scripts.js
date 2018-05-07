db71f094a2eeae70e805cb1132b83ca047b9943d
Florah Masakhwe <masakhweflosh@gmail.com>
2018-05-07 10:35:31 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,32 @@
 //business logic
 var pingPongNumbers = [];
-
-function pingPong (num){
-    for (var index = 1; index <= num; index+=1){
+  function pingPongNumber(num){
+    for (var index = 1; index <= num; index ++){
         if(index % 3 === 0){
-            pingPongNumbers.push("pingpong");
+            pingPongNumbers.push("ping");
+        } else if(index % 5 === 0){
+           pingPongNumbers.push("pong")
+        } else if (index % 15 === 0){
+           pingPongNumbers.push("pingpong")
+        } else{
+            pingPongNumbers.push( index)
         }
+          return pingPongNumbers;
       }
     }
-
 
 
 
 //user interface logic
 
 $(document).ready(function(){
-   $("form#game").submit(function(){
-      event.preventDefault();
+   $("form#game").submit(function(event){
+     $("#result").empty();
       var num = parseInt($("input#number").val());
-
-      pingPong(num);
-
-            pingPongNumbers.forEach(function(num){
-         $("#result").append('<li>' + num + "</li>");
-         alert{result};
+      var pingPongNumbers = pingPongNumber(num);
+      pingPongNumbers.forEach(function(pingPongNumber){
+         $("ul#result").append('<li>' + num + "</li>");
+         event.preventDefault();
       });
    });
 });
